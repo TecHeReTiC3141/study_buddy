71759d7f83956371b0430087c65a1057b98873a8
TecHeReTiC <talex73@mail.ru>
2023-07-12 23:22:51 +0500

    Basic pages have been created + db seems to be connected

--- base/views.py
+++ base/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'JS'},
+    {'id': 3, 'name': 'C++'},
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+
+    r = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            r = i
+            break
+
+    print(r)
+    context = {'room': r}
+    return render(request, 'base/room.html', context)
--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "base.apps.BaseConfig",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -76,8 +78,11 @@ WSGI_APPLICATION = "study_buddy.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'django_db',
+        "USER": 'root',
+        "PASSWORD": 'TecHeres3141',
+        "HOST": 'localhost',
     }
 }
 
